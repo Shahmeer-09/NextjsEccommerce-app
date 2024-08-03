e3c4507e07416b6d9510064761319c3a7830bc40
Shahmeer-09 <shahmeerweb@gmail.com>
2024-08-03 18:00:39 +0500

    error page

---
+++ app/(storefront)/product/[id]/error.tsx
@@ -0,0 +1,9 @@
+export default function Error(){
+    return(
+        <div className=" flex items-center justify-center h  ">
+            <h1 className="text-3xl font-bold text-red-500" >Error:</h1>
+            <p className="text-3xl font-bold text-red-500" >Something wen wrong... please login or try again</p>
+        </div>
+    
+    )
+}
